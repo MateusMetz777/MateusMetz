affa3418b023b688be7f47c113fd8f730ecddb4c
MateusMetz777 <mateusavilametz@gmail.com>
2024-08-30 14:27:25 -0300

    Add files via upload

---
+++ exercicio2.js
@@ -0,0 +1,28 @@
+//Crie uma variável que receba e armazene a sua idade no tipo number
+
+let idade = 18
+
+
+//Crie uma outra variável que receba o valor da sua primeira variável, mas com o seu tipo alterado para string
+
+let idadeTipoString = idade.toString()
+
+
+/*Imprima duas mensagens no terminal:
+
+A primeira deve mostrar o valor e o tipo da primeira variável
+
+A segunda deve mostrar o valor e o tipo da segunda variável*/
+
+console.log(idade, typeof idade)
+console.log(idadeTipoString, typeof idadeTipoString)
+
+
+
+
+
+
+
+
+
+
