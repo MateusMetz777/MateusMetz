ff5e651dc8c227590a21a3e025df9c0fc89b0a5a
MateusMetz777 <mateusavilametz@gmail.com>
2024-09-02 16:50:50 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,90 @@
+//1
+let x = 4
+let y = 3
+resultado = x + y
+console.log(resultado)
+
+ //2
+let equacao = 3*5/2
+console.log(equacao)
+//3
+let multiplicacao = (4 - 5) * -1
+
+console.log(multiplicacao)
+
+//4
+let modulo = 234 % 5
+console.log(modulo)
+
+
+//exercicio2
+
+//1
+
+let brent = 5
+let faiyaz = 5
+console.log (brent===faiyaz)
+
+//2
+
+console.log (brent!==faiyaz)
+
+//3
+
+console.log (brent>faiyaz)
+
+//4
+
+console.log (brent<faiyaz)
+
+
+//exercicio3  and
+let a = true
+let b = false
+let c = true
+console.log(a&&b)
+console.log(b&&c)
+console.log(a&&c)
+console.log(a&&b&&c)
+
+
+//exercicio4    ou/or
+console.log(a||b)
+
+console.log(b||c) 
+
+console.log(a||c)
+
+console.log(a||b||c) 
+
+
+//not
+
+!true
+!false
+console.log(!false)
+console.log(!true)
+
+//exercicio5
+
+let nome = "Mateus Metz" 
+console.log("Nome", nome)
+
+let anoDeNascimento = 2006
+console.log(anoDeNascimento)
+
+let anoAtual = 2024
+console.log(anoAtual)
+
+let idade = 18 
+console.log(idade)
+
+let maiorDeIdade = idade>=18 
+console.log(idade>=18)
+
+let ano2050 = 2050 - 2006
+console.log(ano2050)
+
+
+
+
